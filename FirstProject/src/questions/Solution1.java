61a3a705c2a49210ea905264ba36c37947f15ebc
Anmol Arora <anmolarora1711@gmail.com>
2020-07-27 15:04:48 +0530

    Code

--- FirstProject/src/questions/Solution1.java
+++ FirstProject/src/questions/Solution1.java
@@ -1,9 +1,11 @@
 package questions;
 
+import java.util.Arrays;
+
 public class Solution1 {
 
-	public static void main(String[] args) {
-		String s = "aaabbcccddeeeff"; // -> b // input from scanner
+	/*public static void main(String[] args) {
+		String s = "aaabcccdeeef"; // -> b // input from scanner
 //		String s = "aaabbcccdeeef"; // -> d
 //		String s = "aaabbcccddeeef"; // -> f
 //		String s = "aaabbcccddeeeff"; // -> _
@@ -31,9 +33,43 @@ public class Solution1 {
 			
 		}
 		
+//		Big O notation -> n * n -> n^2 -> 2n -> 3n
+		
 		if(!isNonDuplicateFound) {
 			System.out.println("_");
 		}
+	}*/
+	
+	public static void main(String[] args) {
+		String s = "qklqnknqnldckn";
+		
+		int arr[] = new int[26];
+		boolean isNonDuplicateFound = false;
+		
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			int asciiCode = c;
+			int index = asciiCode - 'a';
+			arr[index]++;
+		}
+		
+		for(int i = 0; i < s.length(); i++) {
+			char c = s.charAt(i);
+			int asciiCode = c;
+			int index = asciiCode - 'a';
+			int value = arr[index];
+			if(value == 1) {
+				System.out.println(c);
+				isNonDuplicateFound = true;
+				break;
+			}
+		}
+		
+		if(!isNonDuplicateFound) {
+			System.out.println("_");
+		}
+		
+		System.out.println(Arrays.toString(arr));
 	}
 
 }
