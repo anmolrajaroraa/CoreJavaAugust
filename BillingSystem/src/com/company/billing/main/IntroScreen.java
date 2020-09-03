41bdab7a4818bbb6a0ad4e0ffb8549d8b65761be
Anmol Arora <anmolarora1711@gmail.com>
2020-09-03 13:27:44 +0530

    Code

--- BillingSystem/src/com/company/billing/main/IntroScreen.java
+++ BillingSystem/src/com/company/billing/main/IntroScreen.java
@@ -1,14 +1,22 @@
 package com.company.billing.main;
 
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+import javax.swing.Timer;
 
 public class IntroScreen extends JFrame {
 
 	private static final long serialVersionUID = 4555923535542320691L;
 	private JPanel contentPane;
+	JProgressBar progressBar;
 
 	/**
 	 * Launch the application.
@@ -16,6 +24,7 @@ public class IntroScreen extends JFrame {
 	public static void main(String[] args) {
 		IntroScreen frame = new IntroScreen();
 		frame.setVisible(true);
+		frame.doProgress();
 	}
 
 	/**
@@ -23,15 +32,45 @@ public class IntroScreen extends JFrame {
 	 */
 	public IntroScreen() {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setBounds(5000, 5000, 450, 300);
+		this.setBounds(5000, 5000, 620, 320);
 		this.setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setLayout(null);
+		contentPane.setBackground(new Color(18, 77, 153));
 		this.setContentPane(contentPane);
 		
-		JLabel lblBgImage = new JLabel("");
-		lblBgImage.setBounds(0, 0, 450, 278);
-		lblBgImage.setIcon(new ImageIcon( IntroScreen.class.getResource("/com/company/billing/main/introbg.jpg") ));
-		contentPane.add(lblBgImage);
+		JLabel lblImageBg = new JLabel("");
+		lblImageBg.setBounds(0, 0, 620, 278);
+		lblImageBg.setIcon(new ImageIcon( IntroScreen.class.getResource("/com/company/billing/main/introbg.jpg") ));
+		contentPane.add(lblImageBg);
+		
+		progressBar = new JProgressBar();
+		progressBar.setBounds(0, 278, 620, 20);
+		progressBar.setStringPainted(true);
+		contentPane.add(progressBar);
+		
+//		Class -> cons private -> no object creation possible
+//		Our class already has an object of Class class
+	}
+	
+	int count = 0;
+	Timer timer;
+	
+	private void doProgress() {
+	
+		timer = new Timer(50, new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				count++;
+				progressBar.setValue(count);
+				if(count >= 100) {
+					timer.stop();
+				}
+			}
+		});
+		timer.start();
 	}
 }
+
+// "/com/company/billing/main/introbg.jpg"
